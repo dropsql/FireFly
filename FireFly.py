84a80f92abf909d6f29daccbd173fdf6ed8fc033
dropsql <dropsql@protonmail.com>
2021-02-27 01:21:50 +0100

    code improvement

--- FireFly.py
+++ FireFly.py
@@ -1,8 +1,7 @@
-API_KEY = 'e3ee9044e4527decd1f483bdcafc878b'
-API_URL = 'http://apilayer.net/api/validate?access_key={}&number={}&country_code=&format=1'
+API_KEY = ''
+API_URL = 'http://apilayer.net/api/validate?access_key={key}&number={phone_number}&country_code=&format={format}'
 
-import requests
-import sys
+import requests, sys
 
 from rich.console import Console
 
@@ -16,38 +15,35 @@ BANNER = '''[magenta]
 '''
 
 console = Console()
-
 console.print(BANNER)
 
 if len(sys.argv) == 1:
 	console.print(f'py {sys.argv[0]} <number 1> <number 2> (etc.)')
 	sys.exit(-1)
 
-numbers = [x.strip() for x in sys.argv[1:]]
-
-for number in numbers:
+for number in sys.argv[1:]:
 	console.rule(f'results for number: \'{number}\'')
-	r = requests.get(API_URL.format(API_KEY, number))
+
+	r = requests.get(
+		url=API_URL.format(key=API_KEY, phone_number=number, format=1)
+	)
 	
 	if 'You have not supplied a valid API Access Key' in r.text:
 		console.print('[red][-][/red] your API key seems invalid :/')
 		sys.exit()
 
 	json_data = r.json()
+
 	if json_data['valid'] == True:
 		for key, value in json_data.items():
 			if value:
-				key = key.replace('_', ' ')
-
-				if ' ' in key:
-					x = key.split(' ')
+				if '_' in key:
 					out = ''
-					for y in x:
-						out += y[0].upper() + y[1:] + ' '
-					key = out
+					for y in key.split('_'):
+						out += f'{y[0].upper()}{y[1:]} '
+					out = out[:-1] if out.endswith(' ') else out
 				else:
-					key = key[0].upper() + key[1:]
-
-				console.print(f'[green][+][/green] {key}: {value}')
+					out = f'{key[0].upper()}{key[1:]}'
+				console.print(f'[green][+][/green] {out}: {value}')
 	else:
 		console.print('[red][-][/red] the phone number seems invalid :/')
